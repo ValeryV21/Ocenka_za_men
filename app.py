49941481341ad17a0ee1da4506b59dc92debbafc
ValeryV21 <valeryvangelov5@gmail.com>
2026-01-13 12:13:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,58 @@
 import streamlit as st
-import pandas as pd
 
-st.title("📊 Любими неща – класна анкета")
+st.title("🎬 Филми по жанр")
 
-if "colors" not in st.session_state:
-    st.session_state.colors = {
-        "Червен": 0,
-        "Син": 0,
-        "Зелен": 0,
-        "Жълт": 0
-    }
+# Речник: жанр → списък с филми
+movies = {
+    "Екшън": [
+        "Джон Уик",
+        "Лудият Макс: Пътят на яростта",
+        "Умирай трудно",
+        "Гладиатор",
+        "Черният рицар"
+    ],
+    "Ужаси": [
+        "Заклинанието",
+        "То",
+        "Предизвестена смърт",
+        "Коварен",
+        "Кошмар на Елм стрийт"
+    ],
+    "Комедия": [
+        "Сам вкъщи",
+        "Ергенският запой",
+        "От глупав по-глупав",
+        "Почивката на Мистър Бийн",
+        "Големи момчета"
+    ],
+    "Научна фантастика": [
+        "Матрицата",
+        "Генезис",
+        "Интерстелар",
+        "Аватар",
+        "Терминатор"
+    ],
+    "Романтичен": [
+        "Титаник",
+        "Тетрадката",
+        "Ла Ла Ленд",
+        "Гордост и предразсъдъци",
+        "Здрач"
+    ],
+    "Фентъзи": [
+        "Хари Потър",
+        "Властелинът на пръстените",
+        "Хобит",
+        "Хрониките на Нарния",
+        "Пърси Джаксън"
+    ]
+}
 
-if "sports" not in st.session_state:
-    st.session_state.sports = {
-        "Футбол": 0,
-        "Баскетбол": 0,
-        "Волейбол": 0,
-        "Плуване": 0
-    }
+# Избор на жанр
+genre = st.selectbox("Избери жанр:", list(movies.keys()))
 
-st.subheader("Избери любими неща")
-
-color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
-sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
-
-if st.button("Запази избора"):
-    st.session_state.colors[color] += 1
-    st.session_state.sports[sport] += 1
-    st.success("Изборът е записан!")
-
-st.subheader("📈 Резултати")
-
-colors_df = pd.DataFrame.from_dict(
-    st.session_state.colors, orient="index", columns=["Брой"]
-)
-st.bar_chart(colors_df)
-
-sports_df = pd.DataFrame.from_dict(
-    st.session_state.sports, orient="index", columns=["Брой"]
-)
-st.bar_chart(sports_df)
+# Показване на филмите
+st.subheader("🎥 Филми в този жанр:")
+for movie in movies[genre]:
+    st.write("•", movie)
 
